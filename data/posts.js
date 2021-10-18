090c027706ee7b85f80c8dfe67dd0719cd2e61b7
jawad <jawadhalbymakary@gmail.com>
2021-10-18 19:45:56 +0300

    added post data

---
+++ data/posts.js
@@ -0,0 +1,22 @@
+import { USERS } from "./users";
+
+export const POSTS = [
+  {
+    imageUrl:
+      "https://img.xcitefun.net/users/2013/11/345649,xcitefun-snake-river-6.jpg",
+    users: USERS[0].user,
+    likes: 100,
+    caption: "lorem ipsum",
+    profile_picture: USERS[0].image,
+    comments: [
+      {
+        user: "test",
+        comment: "test123",
+      },
+      {
+        user: "test",
+        comment: "test123",
+      },
+    ],
+  },
+];
