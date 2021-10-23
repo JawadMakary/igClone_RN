300334f0bc0b04a78903265152956ef1a8e992d0
jawad <jawadhalbymakary@gmail.com>
2021-10-24 00:57:57 +0300

    bottomTab is done

--- components/Home/BottomTabs.js
+++ components/Home/BottomTabs.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Divider } from "react-native-elements";
 
 export const bottomTabsIcons = [
@@ -39,11 +39,47 @@ export const bottomTabsIcons = [
   },
 ];
 export default function BottomTabs({ icons }) {
+  const [activeTab, setActiveTab] = useState("Home");
+  const Icon = ({ icon }) => (
+    <TouchableOpacity onPress={() => setActiveTab(icon.name)}>
+      <Image
+        source={{ uri: activeTab === icon.name ? icon.active : icon.inactive }}
+        style={[styles.icon, icon.name == "Profile" ? styles.profilePic : null]}
+      />
+    </TouchableOpacity>
+  );
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.wrapper}>
+      <Divider width={1} orientation="vertical" />
+      <View style={styles.container}>
+        {icons.map((icon, index) => (
+          <Icon key={index} icon={icon} />
+        ))}
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    position: "absolute",
+    width: "100%",
+    bottom: "0%",
+    zIndex: 999,
+    backgroundColor: "#000",
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    height: 50,
+    paddingTop: 10,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+  },
+  profilePic: {
+    borderRadius: 50,
+    borderColor: "#fff",
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, SafeAreaView, StyleSheet, ScrollView } from "react-native";
+import BottomTabs, { bottomTabsIcons } from "../components/Home/BottomTabs";
 import Header from "../components/Home/Header";
 import Post from "../components/Home/Post";
 import Stories from "../components/Home/Stories";
@@ -15,6 +16,7 @@ export default function HomeScreen() {
           <Post post={post} key={index} />
         ))}
       </ScrollView>
+      <BottomTabs icons={bottomTabsIcons} />
     </SafeAreaView>
   );
 }
